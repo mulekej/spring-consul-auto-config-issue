8047376a03ac73a8cc1918b0453ddd923d6e011b
mulekej <ejmulek@gmail.com>
2020-07-02 12:09:49 -0500

    Initial commit

---
+++ src/main/groovy/com/ericmulek/springconsulautoconfigissue/SpringConsulAutoConfigIssueApplication.groovy
@@ -0,0 +1,13 @@
+package com.ericmulek.springconsulautoconfigissue
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringConsulAutoConfigIssueApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringConsulAutoConfigIssueApplication, args)
+    }
+
+}
---
+++ src/test/groovy/com/ericmulek/springconsulautoconfigissue/SpringConsulAutoConfigIssueApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.ericmulek.springconsulautoconfigissue
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringConsulAutoConfigIssueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
